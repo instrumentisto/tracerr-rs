b87d44006053ddf80d52f8a7eff936ebd23957c6
tyranron <tyranron@gmail.com>
2021-06-24 17:10:06 +0300

    Improve rustc lints

--- src/lib.rs
+++ src/lib.rs
@@ -43,17 +43,18 @@
 //! ```
 
 #![deny(
-    broken_intra_doc_links,
-    missing_debug_implementations,
     nonstandard_style,
     rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
+    rustdoc::private_intra_doc_links,
     trivial_casts,
     trivial_numeric_casts
 )]
-#![forbid(unsafe_code)]
+#![forbid(non_ascii_idents, unsafe_code)]
 #![warn(
     deprecated_in_future,
     missing_copy_implementations,
+    missing_debug_implementations,
     missing_docs,
     unreachable_pub,
     unused_import_braces,
